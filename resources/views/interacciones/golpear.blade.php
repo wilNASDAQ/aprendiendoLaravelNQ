a9f92492982b368d6cb7e928ee06ad513e091434
Wilmer Iriarte <Wilmixmer@gmail.com>
2026-02-06 01:49:56 -0500

    creacion de base de datos usuarios en migrations

--- resources/views/interacciones/golpear.blade.php
+++ resources/views/interacciones/golpear.blade.php
@@ -1,20 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
-    <title>GOLPE</title>
-</head>
-<body>
-    <div class="max-w-7xl mx-auto px-4">
+@extends('layouts.deafultView')
 
-        <x-alerta-logica title="GOLPE" type="danger">
-            TE GOLPEE JAJAJAJA
-        </x-alerta-logica>
+@section('title', 'Golpear')
 
-        <h1>cosas</h1>
+@section('content')
 
-</body>
-</html>
+    <x-alerta-logica title="GOLPE" type="danger">
+        TE GOLPEE JAJAJAJA
+    </x-alerta-logica>
+
+    <h1>cosas</h1>
+
+@endsection
